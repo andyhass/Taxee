ab5e147e239c69e506e487b641ae3d533a88e1dc
Andy Hass <andy.hass@powerreviews.com>
2016-04-25 18:38:14 -0500

    Enabling 2015, 2016 data in the API.

--- models/TaxCalculator.php
+++ models/TaxCalculator.php
@@ -6,5 +6,5 @@
 class TaxCalculatorModel {
     public $ssa_rate       = 0.062;
     public $medicare_rate  = 0.0145;
-    public $supported_years = array(2014);
+    public $supported_years = array(2014, 2015, 2016);
 }
